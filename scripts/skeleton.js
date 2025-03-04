ca31c8ee97a64aaeec011ba10597a5faa0e22afe
Allen Rosales <arosales9@my.bcit.ca>
2025-03-04 11:36:33 -0800

    demo09

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,7 +1,3 @@
-//---------------------------------------------------
-// This function loads the parts of your skeleton 
-// (navbar, footer, and other things) into html doc. 
-//---------------------------------------------------
 function loadSkeleton() {
 
     firebase.auth().onAuthStateChanged(function (user) {
@@ -10,11 +6,12 @@ function loadSkeleton() {
             // User is signed in.
             // Do something for the user here.
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
         } else {
             // No user is signed in.
             console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
         }
-        console.log($('#footerPlaceholder').load('./text/footer.html'));
     });
 }
-loadSkeleton(); //invoke the function
+loadSkeleton();
